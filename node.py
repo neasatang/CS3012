a69c379bb2dec7d1b7381b94fb5e4da4d2766d4c
Neasa Tang <neasatang@Neasas-MacBook-Pro.local>
2017-10-07 15:12:34 +0100

    fixing git

--- node.py
+++ node.py
@@ -1,5 +1,12 @@
+#Binary Tree Node class
 class BTNode (object):
+
+    #Constructor for a node
      def __init__(self,val):
          self.val = val
          self.right = None
          self.left = None
+
+
+def printHello():
+    print("hi")
