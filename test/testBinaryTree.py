5fb2e3011b56b018bf751a2ba11380c12a88bf2f
Neasa Tang <tangn@tcd.ie>
2017-10-18 23:35:38 +0100

    added small test case

--- test/testBinaryTree.py
+++ test/testBinaryTree.py
@@ -1,10 +1,15 @@
+import binarytree
 import unittest
-from binaryTree import binaryTree
-
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    def test_LCA(self):
+
+        tree = binarytree.Tree()
+        tree.put(2)
+        tree.put(5)
+        tree.put(3)
+        self.assertEqual(tree.find_common(5,3),2)
 
 
 if __name__ == '__main__':
