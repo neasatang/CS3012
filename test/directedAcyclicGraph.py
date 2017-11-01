80eee9ecd701182a17afb1151b1f3381ce2f6062
Neasa Tang <tangn@tcd.ie>
2017-11-01 11:08:59 +0000

    added comments to code to make it readable

--- test/directedAcyclicGraph.py
+++ test/directedAcyclicGraph.py
@@ -1,9 +1,11 @@
 import sys
 class DAG(object):
 
+    # prints graph
     def print_graph(self, graph):
         print(graph)
 
+    # prints the nodes from a list from graph
     def print_node_list(self,list):
         print(list)
 
@@ -36,13 +38,14 @@ class DAG(object):
         else:
             raise KeyError("One or both nodes do not exist")
 
+    # function that does DFS
     def DFS(self, node_list, graph, node):
         if not graph[node]:
             return True
         else:
             for child in graph[node]:
                 if child not in node_list:
-                    node_list.append(child)
+                    node_list.append(child) #Add to list that stores the route
                     if not self.DFS(node_list, graph, child):
                         return False
                     node_list.remove(child)
@@ -50,6 +53,7 @@ class DAG(object):
                     return False
             return True
 
+    # wrapper for DFS function
     def DFSWrapper(self, graph):
         result = True
         for node in graph:
@@ -59,37 +63,37 @@ class DAG(object):
 
         return result
 
+    # wrapper for calculating the LCA using DFS - similar to DFS() but modified code to get LCA to work
     def LCA_DFS_Wrapper(self, graph, nodeA, nodeB):
         global node_A_list
         node_A_list = []
         global node_B_list
         node_B_list = []
 
-
+        #gets the node routes for each node and stores them into a list
         for node in graph:
             self.LCA_DFS([node], graph, node, 1, nodeA)
             self.LCA_DFS([node], graph, node, 2, nodeB)
 
-        self.print_node_list(node_A_list)
-        self.print_node_list(node_B_list)
-
         lowest_count = sys.maxsize
         for itemA in node_A_list:
             for itemB in node_B_list:
-                count = 0
+                count = 0 # count increments per route until node is found
                 for index, node1 in enumerate(reversed(itemA)):
                     count = index
                     for node2 in reversed(itemB):
-                        if node1 == node2 and count < lowest_count:
+                        if node1 == node2 and count < lowest_count: # LCA is the one with the lowest count
                             LCANode = node2
                             lowest_count = count
                             break
                         count += 1
-        print(LCANode)
+        #print(LCANode) #testing purposes
 
-
+    # calculates the DFS for an LCA node
     def LCA_DFS(self, node_list, graph, node, index, end_node):
         if node == end_node:
+
+            # distinguish between the two routes using index
             if index == 1:
                 node_A_list.append(node_list[:])
             elif index == 2:
@@ -102,7 +106,7 @@ class DAG(object):
         else:
             for child in graph[node]:
                 if child not in node_list:
-                    node_list.append(child)
+                    node_list.append(child) # appends child node to correct node list using the index parameter
                     if not self.LCA_DFS(node_list, graph, child, index, end_node):
                         return False
                     node_list.remove(child)
