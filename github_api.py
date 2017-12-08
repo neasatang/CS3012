42c43be1291eaba1c27999aacb275049e0b88d54
Neasa Tang <tangn@tcd.ie>
2017-12-08 17:20:47 +0000

    gets a list of users from X year

--- github_api.py
+++ github_api.py
@@ -1,7 +1,22 @@
-from github import Github
 import sys
+from github import Github
 
-g = Github(sys.argv[1], sys.argv[2])
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
 
-for repo in g.get_user().get_repos():
-    print(repo.name)
+redirect_uri = "http://127.0.0.1:5000/callback"
+
+@app.route("/")
+def index():
+    g = Github(sys.argv[1], sys.argv[2])
+    list=""
+
+    for item in g.get_users(since=2017):
+        print(item)
+
+    print(list)
+
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(host = '127.0.0.1') 
